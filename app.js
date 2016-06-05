bf2566f40a4df8fee01882909d8bc7ebd6dea630
flipthebitcoin <tnbnoob1@hotmail.com>
2016-06-05 16:10:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -798,7 +798,7 @@ var Navbar = React.createClass({
           {className: 'navbar-header'},
           el.a({className: 'navbar-brand', href:'/'},
           el.span(
-                el.div33( className: 'div33')
+                el.div33({className: 'div33'})
               )
           )
         ),
